ef4c826a57e0a4db97c9d3d09494d776b5c3f64f
Sebastian Aviña <sebastianavina@gmail.com>
2014-02-12 22:42:01 -0600

    Script para generar base de datos

---
+++ generar_bd.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+from vault import dir_vault
+
+import shutil
+
+shutil.delete(dir_vault + "facturas.db")
+conn = sqlite3.connect(dir_vault + "facturas.db")
+
+c = conn.cursor()
+
+c.execute("CREATE TABLE facturas (id INTEGER PRIMARY KEY, cfdi, emisor, numero_factura)")
+c.execute("CREATE UNIQUE INDEX facturas_id ON facturas(id ASC)")
+c.execute("CREATE TABLE conceptos (id INTEGER PRIMARY KEY, cantidad NUMERIC, noIdentificacion TEXT, descripcion TEXT, valorUnitario TEXT, importe NUMERIC)")
+c.execute("CREATE INDEX conceptos_id ON conceptos(id ASC);")
