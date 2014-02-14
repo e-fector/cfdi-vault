fae564aad66fdad62a51731d4adbb28fced98b0a
Sebastian Aviña <sebastianavina@gmail.com>
2014-02-13 21:56:55 -0600

    Soporte para CFDi

--- factura_xml.py
+++ factura_xml.py
@@ -11,10 +11,17 @@ class Factura:
 		#codecs.open(self.xml, encoding="UTF-8"))
 		self.raiz = self.arbol.getroot()
 
-		self.__class__ = FacturaCFD2
+		version = self.raiz.tag.replace("{http://www.sat.gob.mx/cfd/","").\
+				replace("}Comprobante","")
+		
+		if version == "2":
+			self.__class__ = FacturaCFD2
+		elif version == "3":
+			self.__class__ = FacturaCFDi3
+
 		return None
 
-	def GetFecha(self): 
+	def GetFecha(self): 		
 		raise NotImplementedError()
 	def GetConceptos(self):
 		raise NotImplementedError()
@@ -26,7 +33,35 @@ class Factura:
 		raise NotImplementedError()
 	
 
-	
+
+class FacturaCFDi3(Factura):
+	"""
+	CFDi
+	"""
+
+	def __init__(self, xml):
+		pass
+		
+	def GetFecha(self):
+		return self.raiz.attrib["fecha"]
+
+	def GetConceptos(self):
+		conceptos = self.raiz.iter("{http://www.sat.gob.mx/cfd/3}Concepto")
+		return conceptos
+
+	def GetRFC(self): 
+		Emisor = self.raiz.find("{http://www.sat.gob.mx/cfd/3}Emisor")
+		return Emisor.attrib["rfc"]
+
+	def GetFolio(self): 		
+		return self.raiz.attrib["folio"]
+		
+	def GetCFDi(self): 
+		Timbre = self.raiz.find("{http://www.sat.gob.mx/cfd/3}Complemento")
+		a = Timbre.iter("{http://www.sat.gob.mx/TimbreFiscalDigital}TimbreFiscalDigital").next()
+		
+		return a.attrib["UUID"]
+
 
 class FacturaCFD2(Factura):
 	""" 
@@ -34,13 +69,8 @@ class FacturaCFD2(Factura):
 	"""
 	
 	def __init__(self, xml):
-		self.xml = xml
-		self.arbol = ElementTree.parse(self.xml)
-		#codecs.open(self.xml, encoding="UTF-8"))
-		self.raiz = self.arbol.getroot()
-    
-		return None
-	
+		pass
+
 	def GetFecha(self):
 		return self.raiz.attrib["fecha"]
 
