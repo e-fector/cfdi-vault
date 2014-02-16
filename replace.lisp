07856f31f90d4ae42a2e20e69636f7c0fa822ec5
Sebastian Aviña <sebastianavina@gmail.com>
2014-02-15 23:03:50 -0600

    se borro el script de lisp

--- replace.lisp
+++
@@ -1,7 +0,0 @@
-;;(setq directory "/home/sebastianavina/Dropbox/facturas_attachments/")
-
-;;(defun findr-query-replace (directory)
-;;  "Do `query-replace-regexp' of FROM with TO, on each file found by findr.
-
-(find-dired "/home/sebastianavina/Dropbox/facturas_attachments/" "-name \"*.xml\"")
-;;(dired-do-rename-regexp "Ñ" "Ã‘")
